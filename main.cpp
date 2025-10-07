c69a0950e23d4c04a920770e1f480cdbcddf1684
fabiangruter <fabiangruter@gmail.com>
2025-10-07 12:16:26 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/main.cc to edit this template
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: user
+ *
+ * Created on 7. Oktober 2025, 12:13
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
